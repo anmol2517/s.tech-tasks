99a2628aebc36d5429113f4a5af35b92550b8bbe
Anmol Kumar <110680449+anmol2517@users.noreply.github.com>
2026-01-25 17:28:33 +0530

    Add files via upload

--- Node/UnknownRoutes/index.js
+++ Node/UnknownRoutes/index.js
@@ -1,1 +1,44 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+app.get("/products", (req, res) => {
+  res.send("Here is the list of all products.");
+});
+
+app.post("/products", (req, res) => {
+  res.send("A new product has been added.");
+});
+
+app.get("/categories", (req, res) => {
+  res.send("Here is the list of all categories.");
+});
+
+app.post("/categories", (req, res) => {
+  res.send("A new category has been created.");
+});
+
+app.use((req, res) => {
+  res.status(404).send("<h1>404 - Page Not Found</h1>");
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on http://localhost:4000");
+});
+
+/*
+
+Test URLs
+
+GET → http://localhost:4000/products
+
+POST → http://localhost:4000/products
+
+GET → http://localhost:4000/categories
+
+POST → http://localhost:4000/categories
+
+Undefined → http://localhost:4000/random → 404 page
+
+*/
