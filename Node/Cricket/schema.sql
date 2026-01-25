4bf9b9ee81bb847bca099980a3c76f1f49cb47d0
Anmol Kumar <110680449+anmol2517@users.noreply.github.com>
2026-01-26 04:18:43 +0530

    Update schema.sql

--- Node/Cricket/schema.sql
+++ Node/Cricket/schema.sql
@@ -1,10 +1,24 @@
 create database if not exists cricket_careers;
 use cricket_careers;
+
 create table players(
-  id int auto_increment primary key,
-  name varchar(150),
-  matches int,
-  runs int,
-  centuries int,
-  profile text
+id int auto_increment primary key,
+name varchar(150),
+matches int,
+runs int,
+centuries int,
+profile text
 );
+
+create table live_score(
+id int primary key auto_increment,
+team1 varchar(50),
+team2 varchar(50),
+score1 varchar(20),
+score2 varchar(20),
+overs varchar(20)
+);
+
+insert into live_score(team1,team2,score1,score2,overs)
+values('IND','AUS','120/2','118/3','15.2');
+insert into players(name,matches,runs,centuries,profile)
